9dbb481f1959969693888b8f201cb12dcd6292f1
bhakti <bhaktibhanage4@gmail.com>
2023-04-27 01:51:32 +0530

    Using Runnable

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        new Bike("1234",330,"XYZ").start();
+        new Bike("5678",440, "PQR").start();
+        new Bike("3456",321, "GRT").start();
+
+    }
+}
